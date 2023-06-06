6bdd855f85ec3f443465214b1a23a0e53da78689
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-06-06 17:56:48 +0200

    :heavy_plus_sign: Import React Router configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
+// Import dependencies
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+
+// Import React Router config
+import ReactRouter from './routes/ReactRouter/ReactRouter.jsx'
+
+// Import index CSS
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={ReactRouter} />
   </React.StrictMode>,
 )
