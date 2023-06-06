2673de4b068263d0e6e3366605d9f4c1e15dd27d
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-06-06 20:22:24 +0200

    :heavy_plus_sign: Import Heading1 component

--- src/pages/CreateEmployee/CreateEmployee.jsx
+++ src/pages/CreateEmployee/CreateEmployee.jsx
@@ -1,13 +1,18 @@
 // Import dependencies
 import { useEffect } from "react"
 
+// Import components
+import Heading1 from "../../components/Heading1/Heading1";
+
 function CreateEmployee() {
     useEffect(() => {
         document.title = "HRnet - Create Employee";
     }, []);
 
     return (
-        <main>Create Employee</main>
+        <main>
+            <Heading1 h1="Create Employee" />
+        </main>
     )
 }
 
--- src/pages/EmployeeList/EmployeeList.jsx
+++ src/pages/EmployeeList/EmployeeList.jsx
@@ -1,13 +1,18 @@
 // Import dependencies
 import { useEffect } from "react"
 
+// Import components
+import Heading1 from "../../components/Heading1/Heading1";
+
 function EmployeeList() {
     useEffect(() => {
         document.title = "HRnet - Employee List";
     }, []);
 
     return (
-        <main>Employee List</main>
+        <main>
+            <Heading1 h1="Employee List" />
+        </main>
     )
 }
 
