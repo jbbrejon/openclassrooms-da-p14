030c551b360ad08d2d797eedea465f915fcd57e6
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-06-06 21:11:21 +0200

    :construction: Create FormCreateEmployee component

---
+++ src/components/FormCreateEmployee/FormCreateEmployee.jsx
@@ -0,0 +1,15 @@
+// Import CSS module
+import styles from './FormCreateEmployee.module.css'
+
+function FormCreateEmployee() {
+
+    return (
+        <>
+            <form className={styles.form}>
+                Form
+            </form>
+        </>
+    )
+}
+
+export default FormCreateEmployee
