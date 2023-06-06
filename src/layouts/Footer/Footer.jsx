7dea4c59cc0c6c7ed0577ffd1b3b2acbcade860a
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-06-06 19:34:11 +0200

    :sparkles: Customize footer

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,6 +1,9 @@
+// Import CSS module
+import styles from './Footer.module.css'
+
 function Footer() {
     return (
-        <footer>Footer</footer>
+        <footer className={styles.footer}>HRnet - Wealth Health</footer>
     )
 }
 
