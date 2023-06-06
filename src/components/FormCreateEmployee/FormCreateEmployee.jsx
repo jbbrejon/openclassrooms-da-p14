123c58249278466422268a346c9993a07709d4f7
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-06-06 21:32:51 +0200

    :heavy_plus_sign: Import InputStandard component

--- src/components/FormCreateEmployee/FormCreateEmployee.jsx
+++ src/components/FormCreateEmployee/FormCreateEmployee.jsx
@@ -1,12 +1,40 @@
+// Import dependencies
+import { useState } from 'react'
+
+// Import components
+import InputStandard from '../InputStandard/InputStandard';
+
 // Import CSS module
 import styles from './FormCreateEmployee.module.css'
 
 function FormCreateEmployee() {
+    // Local state (Form inputs)
+    const initialState = {
+        firstName: '',
+        lastName: '',
+    }
 
+    const [employee, setEmployee] = useState(initialState);
+    console.log(employee)
     return (
         <>
             <form className={styles.form}>
-                Form
+                <InputStandard
+                    type="text"
+                    label="First Name"
+                    id="first-name"
+                    name="firstName"
+                    value={employee.firstName}
+                    setState={e => { setEmployee({ ...employee, firstName: e.target.value }); }}
+                />
+                <InputStandard
+                    type="text"
+                    label="Last Name"
+                    id="last-name"
+                    name="lastName"
+                    value={employee.lastName}
+                    setState={e => { setEmployee({ ...employee, lastName: e.target.value }); }}
+                />
             </form>
         </>
     )
