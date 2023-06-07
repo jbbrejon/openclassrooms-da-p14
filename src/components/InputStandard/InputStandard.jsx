34d0c27fcc03b0592c090643373f294a5ff83bbe
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-06-07 21:05:11 +0200

    :wrench: Rename prop(setState -> change)

--- src/components/InputStandard/InputStandard.jsx
+++ src/components/InputStandard/InputStandard.jsx
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types';
 // Import CSS module
 import styles from './InputStandard.module.css'
 
-function InputStandard({ label, type, id, name, value, setState }) {
+function InputStandard({ label, type, id, name, value, change }) {
 
     return (
         <>
             <label className={styles.label} htmlFor={id} >{label}</label>
-            <input className={styles.input} type={type} id={id} title={id} name={name} value={value} onChange={setState} />
+            <input className={styles.input} type={type} id={id} title={id} name={name} value={value} onChange={change} />
         </>
     )
 }
@@ -23,7 +23,7 @@ InputStandard.propTypes = {
         PropTypes.string,
         PropTypes.number
     ]),
-    setState: PropTypes.func,
+    change: PropTypes.func,
 };
 
 export default InputStandard
