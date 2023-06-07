16851e63d62b7f2698e11e9c64428a1f5df86340
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-06-07 21:11:02 +0200

    :construction: Create TableEmployees component

---
+++ src/components/TableEmployees/TableEmployees.jsx
@@ -0,0 +1,13 @@
+// Import CSS module
+import styles from './TableEmployees.module.css'
+
+function TableEmployees() {
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default TableEmployees
