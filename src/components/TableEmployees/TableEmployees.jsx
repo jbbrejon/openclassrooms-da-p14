0f792ac82d869724714a532a79d13b047427427a
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-06-07 21:16:15 +0200

    :construction: Import redux store, Implement search

--- src/components/TableEmployees/TableEmployees.jsx
+++ src/components/TableEmployees/TableEmployees.jsx
@@ -1,11 +1,42 @@
+// Import dependencies
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+
+// Import Redux selector
+import { selectEmployeeList } from '../../redux/selectors/selectors'
+
+// Import components
+import InputStandard from '../InputStandard/InputStandard'
+
 // Import CSS module
 import styles from './TableEmployees.module.css'
 
 function TableEmployees() {
+    // Employees from Redux store
+    const employees = useSelector(selectEmployeeList)
+    // Employees from local state (filtered, sorted)
+    const [localEmployees, setLocalEmployees] = useState(employees)
 
+    // Search employees
+    const searchEmployee = (e) => {
+        let searchResult = [];
+        if (e.target.value.length > 0) {
+            searchResult = employees.filter(o =>
+                Object.keys(o).some(k => o[k].toLowerCase().includes(e.target.value.toLowerCase())));
+            setLocalEmployees([...searchResult])
+        }
+        else {
+            setLocalEmployees(employees);
+        }
+    }
+
+    console.log(localEmployees)
     return (
         <>
-
+            <InputStandard
+                type="text" label="Search: " id="search" name="search"
+                change={e => { searchEmployee(e) }}
+            />
         </>
     )
 }
