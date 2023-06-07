f30c5f03934aaef8d253d4f41e044ef4552085d1
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-06-07 17:49:41 +0200

    :heavy_plus_sign: Import Redux dependencies and configuration

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
 // Import React Router config
 import ReactRouter from './routes/ReactRouter/ReactRouter.jsx'
 
-// Import index CSS
+// Import Redux store
+import store from './redux/store/store';
 
+// Import index CSS
 import './index.css'
+
+// Set persistor (Redux Persist)
+let persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={ReactRouter} />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={ReactRouter} />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
