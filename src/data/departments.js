d832acfc6b5cfb9e744d52c17be647f21a15c17d
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-06-09 17:55:21 +0200

    :card_file_box: Import departments list

---
+++ src/data/departments.js
@@ -0,0 +1,26 @@
+const departments = [
+    {
+        "label": "Sales",
+        "name": "sales"
+    },
+    {
+        "label": "Marketing",
+        "name": "marketing"
+    },
+    {
+        "label": "Enginnering",
+        "name": "enginnering"
+    },
+    {
+        "label": "Human Ressources",
+        "name": "HR"
+    },
+
+
+    {
+        "label": "Legal",
+        "name": "legal"
+    }
+];
+
+export default departments
