fd407a4d0cda963c767da157b3268d4c0558272d
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-06-11 19:53:36 +0200

    :gear: Import table length options for Employee list table

---
+++ src/data/tableLengthOptions.js
@@ -0,0 +1,20 @@
+const tableLengthOptions = [
+    {
+        "label": "10",
+        "name": "10"
+    },
+    {
+        "label": "25",
+        "name": "25"
+    },
+    {
+        "label": "50",
+        "name": "50"
+    },
+    {
+        "label": "100",
+        "name": "100"
+    }
+];
+
+export default tableLengthOptions
