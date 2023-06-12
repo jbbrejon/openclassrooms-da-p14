6b9f57cea4a153855d4e293828014d393c10a391
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-06-12 12:31:51 +0200

    :gear: Change key values from string to number

--- src/data/tableLengthOptions.js
+++ src/data/tableLengthOptions.js
@@ -1,19 +1,19 @@
 const tableLengthOptions = [
     {
-        "label": "10",
-        "name": "10"
+        "label": 10,
+        "name": 10
     },
     {
-        "label": "25",
-        "name": "25"
+        "label": 25,
+        "name": 25
     },
     {
-        "label": "50",
-        "name": "50"
+        "label": 50,
+        "name": 50
     },
     {
-        "label": "100",
-        "name": "100"
+        "label": 100,
+        "name": 100
     }
 ];
 
